0902948009146169d3d70205db4ce40046fc610f
Roger Bertan <roger.bertan@gmail.com>
2026-03-30 01:10:42 -0300

    chore: configure initial spring initializr

---
+++ src/main/java/br/com/bertan/finsync/FinsyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.bertan.finsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinsyncApplication.class, args);
+	}
+
+}
